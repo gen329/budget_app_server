f705e6b5515de742a7b50eebb4a2e0897643271d
Genesis Lara <genesislara@pursuit.org>
2023-10-18 18:22:44 -0400

    removes console logs from controller

--- transactions/controllers/transactionsController.js
+++ transactions/controllers/transactionsController.js
@@ -3,13 +3,11 @@ const transactions = express.Router();
 const transactionsData = require('../models/transaction');
 
 transactions.get("/", (req, res) => {
-  console.log("all transactions")
   res.json(transactionsData);
 })
 
 transactions.get("/:index", (req, res) => {
   const { index } = req.params;
-  console.log("SHOWWWWWW ROUTEEEE")
   if (transactionsData[index]) {
     res.status(200).json(transactionsData[index])
   } else {
@@ -18,8 +16,6 @@ transactions.get("/:index", (req, res) => {
 })
 
 transactions.post("/", (req, res) => {
-  console.log("post route was hit !@!@!");
-  console.log(req.body, "<----- sent from user");
   transactionsData.push(req.body);
   res.status(200).json({ status: "OK", payload: transactionsData[transactionsData.length - 1] })
 })
