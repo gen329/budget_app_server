2f7b4ec40b05a2cb69a06c5abea1c80bcb59cbe3
Genesis Lara <genesislara@pursuit.org>
2023-10-18 15:10:33 -0400

    adds index and post

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 app.use(cors());
 
 app.use(express.json());
-app.use("/transaction", transactions);
+app.use("/transactions", transactions);
 
 app.get("/", (req, res) => {
   res.send("Hello World!!");
--- transactions/controllers/transactionsController.js
+++ transactions/controllers/transactionsController.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const transactions = express.Router();
-const transactionsData = require('../models/transaction').default;
+const transactionsData = require('../models/transaction');
 
 transactions.get("/", (req, res) => {
   console.log("all transactions")
   res.json(transactionsData);
+})
+
+transactions.get("/:index", (req, res) => {
+  const { index } = req.params;
+  console.log("SHOWWWWWW ROUTEEEE")
+  if (transactionsData[index]) {
+    res.status(200).json(transactionsData[index])
+  } else {
+    res.status(404).send("No transactions at this index");
+  }
+})
+
+transactions.post("/", (req, res) => {
+  console.log("post route was hit !@!@!");
+  console.log(req.body, "<----- sent from user");
+  transactionsData.push(req.body);
+  res.status(200).json({ status: "OK", payload: transactionsData[transactionsData.length - 1] })
 })
 
 module.exports = transactions;
