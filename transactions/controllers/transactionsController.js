3cd9ffa0b8ffbb5cba679901ec58d60cfda758b3
Genesis Lara <genesislara@pursuit.org>
2023-10-22 19:35:14 -0400

    small change

--- transactions/controllers/transactionsController.js
+++ transactions/controllers/transactionsController.js
@@ -30,13 +30,13 @@ transactions.delete("/:arrayIndex", (req,res) => {
     }
 })
 
-  transactions.put(":/arrayIndex", (req,res) => {
+  transactions.put("/:arrayIndex", (req,res) => {
       const { arrayIndex } = req.params
       if (transactionsData[arrayIndex]){
           transactionsData[arrayIndex] = req.body
           res.status(200).json((transactionsData[arrayIndex]))
       } else {
-          res.status(400).json({error: "Could not find transaction to be updated!"})
+          res.status(404).json({error: "Could not find transaction to be updated!"})
       }
   })
 
